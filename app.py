2c790f0c59b989a76ba3f318747a7fae49175bc9
Ayesha <ayesha.csolutions@gmail.com>
2026-09-13 03:43:26 +0500

    Initialize app.py integration and track package init files

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-main logic code here
+import os
+import streamlit as st
+from openai import OpenAI
+from dotenv import load_dotenv
+from src.orchestrator import run_lexverify_pipeline
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("GROQ_API_KEY")
+)
